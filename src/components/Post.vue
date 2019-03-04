8f682ffd471b386ea6ea5731357c9cfdc771e301
never <never9612@gmail.com>
2019-03-05 06:30:45 +0900

    投稿のスタイル

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <p>{{text}}</p>
+  <div id="post">
+    <p id="post-text">{{text}}</p>
   </div>
 </template>
 <script>
@@ -9,4 +9,18 @@ export default {
   props: ["text"]
 };
 </script>
+<style scoped>
+#post {
+  width: 320px;
+  height: 60px;
+  border: 1px solid gray;
+  margin: 4px;
+  padding: 4px;
+  box-sizing: border-box;
+}
+#post-text {
+  margin: 0;
+  text-align: left;
+}
+</style>
 
